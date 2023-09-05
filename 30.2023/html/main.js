258db746184ce5fe63d695d32fb8cc1d28ca8732
sardi5150 <142203818+sardi5150@users.noreply.github.com>
2023-09-05 11:32:32 +0700

    latiahan pengulangan js menggunakna while dan for

---
+++ 30.2023/html/main.js
@@ -0,0 +1,6 @@
+function kirimnama(){
+    const nama = prompt("nama kamu siapa");
+    prompt ("halo bro apa kabar");
+    prompt ("saya senang mendengarnya ☠ warna kesukaan kamu apa?");
+    prompt ("terima kasih samoa jumpa kembali");
+}
---
+++ 30.2023/scripts/main.js
@@ -0,0 +1,14 @@
+var jumlhAngkot = 10;
+var angkotberoperasi = 8;
+var noAngkot = 1;
+
+while (noAngkot <= angkotberoperasi){
+    console.log('angkot No. ' + noAngkot+ ' beroperasi dengan baik ' );
+
+    noAngkot++;
+
+}
+
+for( noAngkot = angkotberoperasi + 1; noAngkot <= jumlhAngkot; noAngkot++){
+    console.log('angkot No. ' + noAngkot + 'sedang tidak beroperasi.');
+}
